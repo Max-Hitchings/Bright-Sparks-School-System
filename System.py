6e5f596358f4b4ea3475cdd748391d70bfbba9ab
gk41 <max.hitchings@gmail.com>
2021-10-24 02:08:03 +0100

    Added a menu and better formation for REPORT option

--- System.py
+++ System.py
@@ -1,23 +1,45 @@
 import sys
+import json
+import time
 
 
 class System():
     def __init__(self):
+        with open("./data/menu_items.json") as menu_items:
+            self.menu_items = json.load(menu_items)
+
         with open("./data/database.txt", "r") as db:
             self.database = db
             self.student_list = []
             for student in db.read().splitlines():
                 self.student_list.append(student.split(','))
 
+    def menu(self):
+        time.sleep(2)
+        print("Welcome to the menu\nYour options are:")
+        for key in self.menu_items:
+            print(self.menu_items[key])
+
+        options_mapped = [self.option1, self.option2,
+                          self.option3, self.option4, self.quit]
+
+        user_choice = int(input())
+        options_mapped[user_choice-1]()
+
     def findStudent(self, student_id):
+
         for student in self.student_list:
             if student[0] == student_id:
                 return student
 
-    def option1(self, student_id):
-        return ', '.join(self.findStudent(student_id))
+    def option1(self):
+        student_id = input("Enter the student id\n")
+        print(', '.join(self.findStudent(student_id)))
 
-    def option2(self, student_id):
+        self.menu()
+
+    def option2(self):
+        student_id = input("Enter the student id\n")
         student = self.findStudent(student_id)
         subject = input(
             f"what subject do you want to change? ({student[3]}/{student[5]}/{student[7]})\n")
@@ -38,12 +60,14 @@ class System():
             # self.database = new_database
 
         # return student
+        self.menu()
 
     def option3(self):
         print("\nREPORT:\n")
         for student in self.student_list:
-            print(",".join(student))
-        print("\n")
+            print(
+                f"Name: {' '.join(student[1:3])}\nID: {student[0]}\nGrades: {': '.join(student[3:5])}, {': '.join(student[5:7])}, {': '.join(student[7:9])}\n")
+        self.menu()
 
     def option4(self):
         with open("./data/totals.txt", "w") as totals:
@@ -57,6 +81,7 @@ class System():
                 totals.write((",").join(
                     [*student[:3], str(total_score), str(average_score)]))
                 totals.write("\n")
+        self.menu()
 
     def quit(self):
         sys.exit("Goodbye!")
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from System import System
 
 test = System()
 
-print("option 1: ", test.option1("S1"))
-print("option 2: ", test.option2("S1"))
-print("option 3: ", test.option3())
-print("option 4: ", test.option4())
-print("option 4: ", test.quit())
+# print("option 1: ", test.option1("S1"))
+# print("option 2: ", test.option2("S1"))
+# print("option 3: ", test.option3())
+# print("option 4: ", test.option4())
+# print("option 4: ", test.quit())
+
+test.menu()
